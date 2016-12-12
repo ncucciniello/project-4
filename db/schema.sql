914a6247773b7838b2c1320523ac67946d820860
Nico Cucciniello <ncucciniello@gmail.com>
2016-12-12 14:38:06 -0500

    create and seed psql db

---
+++ db/schema.sql
@@ -0,0 +1,21 @@
+BEGIN;
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS art;
+
+
+CREATE TABLE users(
+  user_id SERIAL PRIMARY KEY,
+  username TEXT NOT NULL
+);
+
+CREATE TABLE art(
+  post_id SERIAL NOT NULL,
+  artist TEXT,
+  img_url TEXT,
+  lat DECIMAL(9,6) NOT NULL,
+  lng DECIMAL(9,6) NOT NULL,
+  info TEXT
+);
+
+COMMIT;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (username) VALUES ('Test User1');
+
+
+INSERT INTO art (artist, img_url, lat, lng, info) VALUES ('Nychos', 'http://streetartnyc.org/wp-content/uploads/2016/10/nychos-mural-art-jersey-city.jpg', 40.731520, -74.042812, 'Stay Weird Marshmallow Man');
+INSERT INTO art (artist, img_url, lat, lng, info) VALUES ('Buff Monster', 'https://bizarrebeyondbelief.files.wordpress.com/2016/10/unnamed.jpg?w=662&h=544', 40.731410, -74.047910, 'The Seven Heavenly Virtue. Located on the back side of the building.');
